1700d613ea437370d5587c2b749c52b16fa8febc
samir <samiralurhp@gmail.com>
2020-09-16 07:27:16 +0530

    first commit

---
+++ src/test/java/com/samir/automationframework/pomautomationframework/AppTest.java
@@ -0,0 +1,17 @@
+package com.samir.automationframework.pomautomationframework;
+
+
+import org.testng.*;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("SAmir");
+    }
+}
